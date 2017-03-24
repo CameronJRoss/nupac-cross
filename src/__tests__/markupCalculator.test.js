db85625cc5878a33c7d4f45ad80086146084860c
Cameron J. Ross <contact@cameronjross.com>
2017-03-23 20:42:43 -0400

    Added support for food estimates.

--- src/__tests__/markupCalculator.test.js
+++ src/__tests__/markupCalculator.test.js
@@ -3,15 +3,14 @@
 const materialTypes = require('../materialTypes');
 const markupCalculator = require('../markupCalculator');
 
-
 describe('Markup Calculator', () => {
 
   it('returns the correct total for a food request', () => {
     expect(markupCalculator.calcTotal({
       costInCents: 129999,
       material: materialTypes.FOOD,
-      people: 3
-    })).toBe('159158');
+      numberOfPeople: 3
+    })).toBe(159158);
   });
 
 });
--- src/markupCalculator.js
+++ src/markupCalculator.js
@@ -1,19 +1,39 @@
 'use strict';
 
+const materialTypes = require('./materialTypes');
+
 module.exports = {
 
+  BASE_MARKUP: 0.05,
+
   /**
-   * Calculates an estimate based on item costs, number of people and material type.
+   * Calculates an estimate based on item costs, number of numberOfPeople and material type.
    *
    * @param {object}        params
    * @param {number}        params.costInCents - the cost of the material in cents
    * @param {materialTypes} params.material - the type of material to be estimated
-   * @param {number}        params.people - the number of people required to complete the job
+   * @param {number}        params.numberOfPeople - the number of numberOfPeople required to complete the job
    *
    * @returns {number} the total amount estimated in cents.
    */
-  calcTotal({ costInCents, material, people }) {
-    return null;
+  calcTotal({ costInCents, material, numberOfPeople }) {
+
+    const materialMarkup = this._getMarkupForMaterial(material);
+    const peopleMarkup = this._getMarkupForNumberOfPeople(numberOfPeople);
+
+    const costWithBaseMarkup = costInCents * (1 + this.BASE_MARKUP);
+
+    return Math.round(costWithBaseMarkup * (1 + materialMarkup + peopleMarkup));
+  },
+
+  _getMarkupForMaterial(material) {
+    if (material === materialTypes.FOOD) {
+      return 0.13;
+    }
+  },
+
+  _getMarkupForNumberOfPeople(numberOfPeople) {
+    return numberOfPeople * 0.012;
   }
 
 };
